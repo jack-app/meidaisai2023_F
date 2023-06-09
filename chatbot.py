7075fbed292f190868aef946e19c701185aec075
Makochaaan <arowana326.315@gmail.com>
2023-06-09 22:48:28 +0900

    final commit

--- chatbot.py
+++ chatbot.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv('.env') 
 
 # openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key = os.environ.get("OPENAI_KEY")
+openai.api_key = os.getenv("OPENAI_KEY")
 
 contents = ["",
             "次に述べるキャラクターになりきって会話してください。君は宇宙に住んでいる宇宙猫フェリセット。性格は気分屋。口調は柔らか。宇宙に一番最初に行った宇宙猫で、他の宇宙猫からとても尊敬されている。",
